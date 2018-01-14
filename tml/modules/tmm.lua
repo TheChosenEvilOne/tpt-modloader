866ce099b1fa6ec87620e6779f0b84c67d34d4d9
TheChosenEvilOne <tceo-email@protonmail.com>
2018-01-14 13:57:01 +0200

    add TMM module

---
+++ tml/modules/tmm.lua
@@ -0,0 +1,23 @@
+-- The Powder Toy Mod Manager
+-- 
+-- This file will be a module for TML which will add UI and mod managing properties
+
+local table metadata = {}
+metadata["name"] = "The Powder Toy Mod Manager"
+metadata["id"] = "tmm"
+metadata["requires"] = "tml:1"
+
+function tmm_getInfo()
+  
+  
+end
+
+function tmm_onLoad()
+
+
+end
+
+function tmm_onDisable()
+
+
+end
