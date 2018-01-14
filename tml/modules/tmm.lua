35a53e16d2db3fef3fffd0df6b1f519003ced83a
TheChosenEvilOne <tceo-email@protonmail.com>
2018-01-14 14:01:52 +0200

    Changes

--- tml/modules/tmm.lua
+++ tml/modules/tmm.lua
@@ -8,16 +8,17 @@ metadata["id"] = "tmm"
 metadata["requires"] = "tml:1"
 
 function tmm_getInfo()
-  
-  
+  return metadata
 end
 
 function tmm_onLoad()
-
-
+  print("NYI")
 end
 
 function tmm_onDisable()
+  print("NYI")
+end
 
-
+local function main()
+  print("NYI")
 end
